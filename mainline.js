db8045adf46c3c8fdea4fe4419309364b25f1b59
Tony Colston <tonetheman@gmail.com>
2019-06-07 16:55:19 -0500

    saving

--- mainline.js
+++ mainline.js
@@ -1,4 +1,5 @@
 let game = null;
+let SCALE = 2;
 
 class MainScene extends Phaser.Scene {
     constructor() {
@@ -27,15 +28,15 @@ class MainScene extends Phaser.Scene {
         // https://0x72.itch.io/2bitcharactergenerator
 
         // load as sprite sheet
-        this.load.spritesheet("a", "aN0406200100_0012.png", {frameWidth : 16,
+        this.load.spritesheet("a", "assets/aN0406200100_0012.png", {frameWidth : 16,
             frameHeight : 16 });
         
         // by hand as atlas
         // picked for front runner sprite
         // 0 and 3 are the images you want
-        this.load.atlas("aa", "0106210504_0012.png", this.runner_json_for_atlas());
-        this.load.atlas("bb", "aN0106020802_0012.png", this.runner_json_for_atlas());
-        this.load.atlas("cc", "aN0304160504_0012.png", this.runner_json_for_atlas());
+        this.load.atlas("aa", "assets/0106210504_0012.png", this.runner_json_for_atlas());
+        this.load.atlas("bb", "assets/aN0106020802_0012.png", this.runner_json_for_atlas());
+        this.load.atlas("cc", "assets/aN0304160504_0012.png", this.runner_json_for_atlas());
     }
     create() {
         // this is a gross way to do this i think...
@@ -50,23 +51,23 @@ class MainScene extends Phaser.Scene {
     
         this.anims.create(config);
         let tmp = this.add.sprite(100,100,"a").play("aAnimation");
-        tmp.setScale(4);
+        tmp.setScale(SCALE);
 
         // shows just the atlas frames
         this.anims.create({ key: 'a2', frames: this.anims.generateFrameNames('aa'), repeat: -1,
             frameRate : 3 });
         let tmp2 = this.add.sprite(200,100,"a").play("a2");
-        tmp2.setScale(4);
+        tmp2.setScale(SCALE);
 
         this.anims.create({ key: 'b2', frames: this.anims.generateFrameNames('bb'), repeat: -1,
         frameRate : 3 });
         let tmp3 = this.add.sprite(100,200,"a").play("b2");
-        tmp3.setScale(4);
+        tmp3.setScale(SCALE);
 
         this.anims.create({ key: 'c2', frames: this.anims.generateFrameNames('cc'), repeat: -1,
         frameRate : 3 });
         let tmp4 = this.add.sprite(200,200,"a").play("c2");
-        tmp4.setScale(4);
+        tmp4.setScale(SCALE);
 
 
     }
